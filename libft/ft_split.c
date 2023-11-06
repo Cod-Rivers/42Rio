2d4c7f939e0423ac7b6c324c7b44adf85535ad6d
riversdev42 <riversdev42@outlook.com>
2023-11-06 15:26:48 -0300

    strtim

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,53 +6,93 @@
 /*   By: rivda-si <rivda-si@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 11:09:04 by rivda-si          #+#    #+#             */
-/*   Updated: 2023/11/06 11:09:54 by rivda-si         ###   ########.fr       */
+/*   Updated: 2023/11/06 13:43:05 by rivda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft" 
+#include "libft.h" 
 
-int ocorrencias (char const *s, char c)
+int ocorrences (char const *s, char c)
 {
-    int j;
+    int len;
     int i;
 
     i = 0;
-    j = 0;
+    len = 0;
     while (s[i] != '\0' && s[i+1] != c)
     {
         if (s[i] == c)
         {
-            j++;
+            len++;
         }
         i++;
     }
-    return (j + 1);
+    return (len + 1);
+}
+static char **allocate(char const *s, char c, char **variable)
+{
+    size_t o;
+    size_t i;
+    size_t len;
+	
+    i = 0;
+    len = 0;
+    o = 0;
+    
+	while (s[i] != '\0')
+    {
+		if (s[i] != c)
+			len++;
+		if (s[i] == c && s[i+1] != c)
+		{
+			variable[o] = (char *) malloc (sizeof(char) * (unsigned int)(len + 1 ));
+		}
+		if(!(variable[o]))
+			return (NULL);
+	}
+	return (variable);
+}
+
+char **fill (char const *s, char c, char **varible)
+{
+	size_t i;
+	size_t **str;
+	size_t o;
+
+	i = 0;
+	str = 0;
+	o = 0;
+	while(variable[i] != c || variable[i] != '\0')
+	{
+		str[o][i] = variable[i];
+		i++
+	}
+	if(variable[i] == c)
+	{
+		str[o][i] = '\0';
+		o++;
+		while(variable[i] == c && variable[i] != '\0')
+			i++;
+	}
+	
+	return (str);
 }
 char **ft_split (char const *s, char c)
 {
-    int o;
-    int i;
-    int j;
-
-    i = 0;
-    j = 0;
-    o = ocorrencias(s, c);
-    char *variaveis[o];
-    while (s[i] != '\0')
-    {
-        while(s[i] != '\0')
-        {
-            if (s[i] == c && [i+1] != c)
-            {
-                i++;
-                o++;
-            }
-            variaveis[j][i] = s[i];
-        }
-    }
+	size_t i;
+	int	 t;
+	char **str;
+	char str2;
+	
+	t = ocurrences(s, c);
+	str2 = allocate(s,c, str);
+	str = (s, c, str)
+	
+	return (str);
 
 }
+
+
 int main (void)
 {
     char const *s[10] = {"parapeito"};
--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,33 +6,68 @@
 /*   By: rivda-si <rivda-si@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 11:11:01 by rivda-si          #+#    #+#             */
-/*   Updated: 2023/11/06 11:11:09 by rivda-si         ###   ########.fr       */
+/*   Updated: 2023/11/06 15:26:15 by rivda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strtrim(char const *s1, char const *set)
+char *ft_strtrim(const char*s1, const char *set)
 {
     size_t i;
     size_t  j;
+	size_t	o;
     char    *temp;
     size_t lennew;
 
     i = 0;
+	o = 0;
     j = 0;
     lennew = (ft_strlen (s1) - ft_strlen(set));
     temp = (char *) malloc(sizeof(char)* lennew +1);
     if (!temp)
         return (NULL);
-    while (i < ft_len(s1))
-        if (s1[i] != set[i])
+    while (i < ft_strlen(s1))
+        while (s1[i] == set[j])
         {
-            temp[j] = s1[i];
             i++;
-            j++;
         }
-    i++
+		while (s1[i] != set[j]) 
+		{
+			temp[o] = s1[i];
+			i++;
+			o++;
+			j++;
+    	}
     temp [i] = '\0';
     return (temp);
+
+testar essa
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*result;
+	size_t	i;
+	size_t	len;
+
+	if (!s1 || !set)
+		return (0);
+	i = 0;
+	while (s1[i] && ft_strchr(set, s1[i]))
+		i++;
+	len = ft_strlen(s1) - i;
+	if (len == 0)
+		return (ft_strdup(""));
+	result = ft_substr(s1, i, len);
+	return (result);
 }
+}
+int main (void)
+{
+	const char *str = {"euqueroagua"};
+	const char *str2 = {"u"};
+	char *str3 = ft_strtrim(str, str2);
+	
+	printf("%s", str3);
+	free (str3);
+}
+	
