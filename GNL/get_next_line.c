fb2c92a78c66d70fb24882af4adb03fbf08adcd3
cod-rivers <riversdev42@outlook.com>
2024-01-30 13:03:58 -0300

    updateGit

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rivda-si <rivda-si@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 14:51:58 by rivda-si          #+#    #+#             */
-/*   Updated: 2023/12/20 14:52:02 by rivda-si         ###   ########.fr       */
+/*   Updated: 2024/01/27 15:11:14 by rivda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,7 +57,6 @@ char	*ext_line(char *buffer, int nb_read)
 	return (ext_line_with_break(buffer));
 }
 
-// verifica se tem \n
 int	has_line(char *str, int nb_read)
 {
 	int	n;
@@ -79,7 +78,6 @@ int	has_line(char *str, int nb_read)
 char	*get_next_line(int fd)
 {
 	int			nb_read;
-	int			len_line;
 	char		*line;
 	static char	*buffer;
 
@@ -87,11 +85,10 @@ char	*get_next_line(int fd)
 	line = "";
 	while ((*line == '\0' && buffer && *buffer) || (nb_read > 0 && fd >= 0))
 	{
-		len_line = has_line(buffer, nb_read);
-		if (len_line)
+		if (has_line(buffer, nb_read))
 		{
 			line = ext_line(buffer, nb_read);
-			buffer = remove_ext_line(buffer, len_line);
+			buffer = remove_ext_line(buffer, has_line(buffer, nb_read));
 			return (line);
 		}
 		else
@@ -100,22 +97,78 @@ char	*get_next_line(int fd)
 			nb_read = read(fd, buffer + ft_strlen(buffer), BUFFER_SIZE);
 		}
 	}
-	if (*buffer)
+	if (buffer)
 		free(buffer);
+	buffer = 0;
 	return (NULL);
 }
 
-int	main (void)
+/*#include <fcntl.h>
+#include <stdio.h>
+int main()
 {
-	int		fd;
-	char	buf[256];
-	int		chars_read;
+     int fd = open("text.txt", O_RDONLY);
+     char *a = get_next_line(fd);
+     printf("primeira chamada: %s", a);
+     free(a);
 
-	fd = open("text.txt", O_RDONLY);
+     char *b = get_next_line(fd);
+     printf("segunda chamada: %s", b);
+     free(b);
 
-		ft_printf("buf %s",get_next_line(fd));
-		ft_printf("buf %s",get_next_line(fd));
-		ft_printf("buf %s",get_next_line(fd));
-	
-	return (0);
-}
+     char *c = get_next_line(fd);
+     printf("terceira chamada: %s", c);
+     free(c);
+
+     char *d = get_next_line(fd);
+     printf("quarta chamada: %s", d);
+     free(d);
+
+     char *e = get_next_line(fd);
+     printf("5 chamada: %s", e);
+     free(e);
+
+     char *f = get_next_line(fd);
+     printf("6 chamada: %s", f);
+     free(f);
+
+     char *g = get_next_line(fd);
+     printf("7 chamada: %s", g);
+     free(g);
+
+     char *h = get_next_line(fd);
+     printf("8 chamada: %s", h);
+     free(h);
+
+     char *i = get_next_line(fd);
+     printf("9 chamada: %s", i);
+     free(i);
+
+     char *j = get_next_line(fd);
+     printf("10 chamada: %s", j);
+     free(j);
+
+     char *k = get_next_line(fd);
+     printf("11 chamada: %s", k);
+     free(k);
+
+     char *l = get_next_line(fd);
+     printf("12 chamada: %s", l);
+     free(l);
+
+     char *m = get_next_line(fd);
+     printf("13 chamada: %s", m);
+     free(m);
+
+     char *n = get_next_line(fd);
+     printf("décima quinta chamada: %s", n);
+     free(n);
+
+     char *o = get_next_line(fd);
+     printf("14 chamada: %s", o);
+     free(o);
+
+     close(fd);
+
+     return 0;
+}*/
